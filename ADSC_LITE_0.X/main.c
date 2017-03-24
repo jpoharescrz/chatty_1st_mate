5fb964655ef1d58be0799313848c34dab730c88f
Dad <Dad@Dad-PC>
2017-03-24 11:45:27 -0700

    Initial commit

---
+++ ADSC_LITE_0.X/main.c
@@ -0,0 +1,118 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using MPLAB(c) Code Configurator
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - pic24-dspic-pic32mm : v1.25
+        Device            :  PIC24FJ128GB204
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.26
+        MPLAB             :  MPLAB X 3.45
+*/
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+// PIC24FJ128GB204 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG4
+#pragma config DSWDTPS = DSWDTPS1F    // Deep Sleep Watchdog Timer Postscale Select bits->1:68719476736 (25.7 Days)
+#pragma config DSWDTOSC = LPRC    // DSWDT Reference Clock Select->DSWDT uses LPRC as reference clock
+#pragma config DSBOREN = ON    // Deep Sleep BOR Enable bit->DSBOR Enabled
+#pragma config DSWDTEN = ON    // Deep Sleep Watchdog Timer Enable->DSWDT Enabled
+#pragma config DSSWEN = ON    // DSEN Bit Enable->Deep Sleep is controlled by the register bit DSEN
+#pragma config PLLDIV = DIVIDE8    // USB 96 MHz PLL Prescaler Select bits->Oscillator input divided by 8 (32 MHz input)
+#pragma config I2C1SEL = DISABLE    // Alternate I2C1 enable bit->I2C1 uses SCL1 and SDA1 pins
+#pragma config IOL1WAY = ON    // PPS IOLOCK Set Only Once Enable bit->Once set, the IOLOCK bit cannot be cleared
+
+// CONFIG3
+#pragma config WPFP = WPFP127           // Write Protection Flash Page Segment Boundary (Page 127 (0x1FC00))
+#pragma config SOSCSEL = OFF            // SOSC Selection bits (Digital (SCLKI) mode)
+#pragma config WDTWIN = PS25_0          // Window Mode Watchdog Timer Window Width Select (Watch Dog Timer Window Width is 25 percent)
+#pragma config PLLSS = PLL_PRI          // PLL Secondary Selection Configuration bit (PLL is fed by the Primary oscillator)
+#pragma config BOREN = ON               // Brown-out Reset Enable (Brown-out Reset Enable)
+#pragma config WPDIS = WPDIS            // Segment Write Protection Disable (Disabled)
+#pragma config WPCFG = WPCFGDIS         // Write Protect Configuration Page Select (Disabled)
+#pragma config WPEND = WPENDMEM         // Segment Write Protection End Page Select (Write Protect from WPFP to the last page of memory)
+
+// CONFIG2
+#pragma config POSCMD = XT              // Primary Oscillator Select (XT Oscillator Enabled)
+//#pragma config POSCMD = EC    // Primary Oscillator Select->External-Clock Mode Enabled
+#pragma config WDTCLK = LPRC    // WDT Clock Source Select bits->WDT uses LPRC
+#pragma config OSCIOFCN = OFF    // OSCO Pin Configuration->OSCO/CLKO/RA3 functions as CLKO (FOSC/2)
+#pragma config FCKSM = CSDCMD    // Clock Switching and Fail-Safe Clock Monitor Configuration bits->Clock switching and Fail-Safe Clock Monitor are disabled
+#pragma config FNOSC = PRIPLL    // Initial Oscillator Select->Primary Oscillator with PLL module (XTPLL,HSPLL, ECPLL)
+#pragma config ALTRB6 = APPEND    // Alternate RB6 pin function enable bit->Append the RP6/ASCL1/PMPD6 functions of RB6 to RA1 pin functions
+#pragma config ALTCMPI = CxINC_RB    // Alternate Comparator Input bit->C1INC is on RB13, C2INC is on RB9 and C3INC is on RA0
+#pragma config WDTCMX = WDTCLK    // WDT Clock Source Select bits->WDT clock source is determined by the WDTCLK Configuration bits
+#pragma config IESO = ON    // Internal External Switchover->Enabled
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler Select (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler Ratio Select (1:128)
+#pragma config WINDIS = OFF             // Windowed WDT Disable (Standard Watchdog Timer)
+#pragma config FWDTEN = OFF            // Watchdog Timer Enable (WDT enabled in hardware)
+#pragma config ICS = PGx3               // Emulator Pin Placement Select bits (Emulator functions are shared with PGEC3/PGED3)
+#pragma config LPCFG = OFF              // Low power regulator control (Disabled - regardless of RETEN)
+#pragma config GWRP = OFF               // General Segment Write Protect (Write to program memory allowed)
+#pragma config GCP = OFF                // General Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF              // JTAG Port Enable (Enabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+#include "mcc_generated_files/mcc.h"
+#include "Core_api.h"
+
+/*
+                         Main application
+ */
+int main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    while (1)
+    {
+        // Start the ADSC code
+        CoreMain();
+    }
+
+    return -1;
+}
+/**
+ End of File
+*/
